d2c7f2c9a367b66e5664ddadc52952ea463cbcd6
Eleni A <eleni.arvanitis@me.com>
2017-12-14 15:19:50 -0500

    initial commit

--- fire/setup.js
+++ fire/setup.js
@@ -1,1 +1,13 @@
-throw new Error('fire/setup.js does not contain a config. You need to run `npm prepare`')
+export default firebase =>
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCSDIJcPHNNOU9aob-hTKJr1Gs2aBfXF6k",
+  authDomain: "elenicodes.firebaseapp.com",
+  databaseURL: "https://elenicodes.firebaseio.com",
+  projectId: "elenicodes",
+  storageBucket: "elenicodes.appspot.com",
+  messagingSenderId: "928734970131"
+});
