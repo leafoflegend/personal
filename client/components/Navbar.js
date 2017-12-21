800eed010d337106cc237f69dbe2730885ae8693
Eleni A <eleni.arvanitis@me.com>
2017-12-21 12:40:36 -0500

    changed to dumb component

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import '~/public/assets/styles/index.css'
 
 const logo = '/assets/img/ea-logo-cream.png';
 
-export default class Navbar extends Component {
-  render() {
-    return <nav className="nav-bar">
-      <NavLink to='/'>
-        <img className="logo" src={logo} alt="ea-logo"/>
-      </NavLink>
-      <NavLink to='/about'>About</NavLink>
-      <NavLink to='/work'>Work</NavLink>
-      <NavLink to='/articles'>Articles</NavLink>
-      <NavLink to='/contact'>Contact</NavLink>
-    </nav>
-  }
+const Navbar = () => {
+  return <nav className="nav-bar">
+    <NavLink to='/'>
+      <img className="logo" src={logo} alt="ea-logo"/>
+    </NavLink>
+    <NavLink to='/about'>About</NavLink>
+    <NavLink to='/work'>Work</NavLink>
+    <NavLink to='/articles'>Articles</NavLink>
+    <NavLink to='/contact'>Contact</NavLink>
+  </nav>
 }
+
+export default Navbar
