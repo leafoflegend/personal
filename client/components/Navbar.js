039bc8e52abdc957c0633de9e09c2471fd62fc0f
Eleni A <eleni.arvanitis@me.com>
2018-01-16 11:04:56 -0500

    removed index.css ref and added future home link possibility

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import '~/public/assets/styles/index.css'
+import '~/public/assets/styles/navbar.css'
 
 const logo = '/assets/img/ea-logo-cream.png';
+
+// TODO: Remove home constant color selection
 
 export default () => {
   return <nav className="nav-bar">
     <NavLink to='/'>
       <img className="logo" src={logo} alt="ea-logo-cream"/>
     </NavLink>
+    {/*<NavLink to='/' className='underline'>Home</NavLink>*/}
     <NavLink to='/about' className='underline'>About</NavLink>
     <NavLink to='/work' className='underline'>Work</NavLink>
     <NavLink to='/articles' className='underline'>Articles</NavLink>
