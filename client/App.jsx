cd63b4ddbafb236021083788af38a803174e3d9b
Eleni A <eleni.arvanitis@me.com>
2018-01-16 10:55:03 -0500

    separated out navbar from new index.css

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 
+import '~/public/assets/styles/index.css'
+import '~/public/assets/styles/button.css'
+
 import Navbar from '~/client/components/Navbar'
 import About from '~/client/components/About'
 import Home from '~/client/components/Home'
+import Work from '~/client/components/Work'
 
 // import firebase from 'firebase'
 
@@ -20,7 +24,7 @@ export default class App extends Component {
         <Switch>
           <Route exact path='/' component={Home}/>
           <Route exact path="/about" component={About}/>
-          {/*<Route exact path='/work' component={Work}/>*/}
+          <Route exact path='/work' component={Work}/>
           {/*<Route exact path='articles' component={Articles}/>*/}
           {/*<Route exact path='/articles/:articleId'*/}
                  {/*component={({match: {params: {articleId}}}) =>*/}
