1c6df9bab2f76bb893d70dcb687a2709e16f4bc9
Eleni A <eleni.arvanitis@me.com>
2018-01-25 11:16:31 -0500

    form validation 50% there

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '~/public/assets/styles/contact.css'
 
-export default () => {
-  return <div className='contact'>
-    <h2>Let's Chat!</h2>
-    <div className='contact-form'>
-      <label className='form-label'>
-        <input className='form-input'
-               type='text'
-               placeholder='Name'/><br/>
-        <input className='form-input'
-               type='email'
-               placeholder='Email'/><br/>
-        <textarea className='form-input'
-                  placeholder='Message'/><br/>
-      </label>
-      <button>Submit</button>
+// TODO: Fix validation
+
+export default class Contact extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div className='contact'>
+      <h2>Let's Chat!</h2>
+      <div className='contact-form'>
+        <label className='form-label'>
+          <input className='form-input'
+                 type='text'
+                 name='name'
+                 placeholder='* Name'
+                 required/><br/>
+          <input className='form-input'
+                 type='email'
+                 name='email'
+                 placeholder='* Email'
+                 required/><br/>
+          <textarea className='form-input'
+                    name='message'
+                    placeholder='* Message'
+                    required/><br/>
+          <button type='submit'>Submit</button>
+        </label>
+      </div>
     </div>
-  </div>
+  }
 }
