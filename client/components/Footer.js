89b73e2dd8a2982049f9de0197caeacd6974cd66
Eleni A <eleni.arvanitis@me.com>
2018-01-27 10:58:04 -0500

    need to separate the items and add padding

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,15 +1,19 @@
 import React from 'react';
+
 import { socialLinks } from "~/secrets"
+import '~/public/assets/styles/footer.css'
 
 export default () => {
     return <div className='footer'>
-      <span>&copy;2018 Eleni Arvanitis</span>
-      {
-        socialLinks.map(link => {
-          return <span key={link[0]}>
-              <a href={link[0]}>{link[1]}</a>
-            </span>
-        })
-      }
+      <span className='copyright'>&copy;2018 Eleni Arvanitis</span>
+      <div className='social'>
+        {
+          socialLinks.map(link => {
+            return <div className='icons' key={link[0]}>
+              <a href={link[0]} target='_blank'>{link[1]}</a>
+            </div>
+          })
+        }
+      </div>
     </div>
 }
