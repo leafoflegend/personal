cabb024f4b71e00690dc441a16fd7ce04d5b5b9d
Eleni A <eleni.arvanitis@me.com>
2018-02-01 16:30:19 -0500

    footer looks :bomb:

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -5,13 +5,13 @@ import '~/public/assets/styles/footer.css'
 
 export default () => {
     return <div className='footer'>
-      <span className='copyright'>&copy;2018 Eleni Arvanitis</span>
-      <div className='social'>
+      <span className='copyright'>&copy; 2018 Eleni Arvanitis</span>
+      <div>
         {
           socialLinks.map(link => {
-            return <div className='icons' key={link[0]}>
-              <a href={link[0]} target='_blank'>{link[1]}</a>
-            </div>
+            return <a key={link[0]} className='icons' href={link[0]} target='_blank'>
+              {link[1]}
+            </a>
           })
         }
       </div>
