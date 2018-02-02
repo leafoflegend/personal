fabdf72c67719a6935271f9248db59807cda804d
Eleni A <eleni.arvanitis@me.com>
2018-02-02 15:16:04 -0500

    moving things around

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,32 +1,42 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+
 import '~/public/assets/styles/home.css'
+import recs from '../recs'
 
 const me = [
-      'a software engineer',
-      'imaginative',
-      'a teaching fellow',
-      'inquisitive',
-      'a gamer',
-      'innovative',
-      'a Ravenclaw',
-      'intuitive',
-    ]
-    , blurb = ``
+  'a software engineer',
+  'imaginative',
+  'a teaching fellow',
+  'inquisitive',
+  'a gamer',
+  'innovative',
+  'a Ravenclaw',
+  'intuitive',
+]
 
 export default () => {
-  return <div className='text-carousel'>
-    I am
+  return <div className='me'>
+    <div className='text-carousel'>
+      I am
         {
           me.map((descriptor, i) => {
-            return <span className='content' key={i}>
+            return <span className='carousel-items' key={i}>
                 {descriptor}
             </span>
           })
         }
-    <p>{blurb}</p>
-    <NavLink to='/contact'>
-      <button>Say hi</button>
-    </NavLink>
+      <NavLink to='/contact'>
+        <button>Say hi</button>
+      </NavLink>
+    </div>
+    <div className='rec'>
+      {
+        recs.map((rec, i) => <div key={i}>
+          <p className='rec-content'>"{rec[0]}"</p>
+          <p className='rec-name'>{rec[1]}</p>
+        </div>)
+      }
+    </div>
   </div>
 }
