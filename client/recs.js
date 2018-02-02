3162b52660e9d33e1d3aec907a621f20b8782d0f
Eleni A <eleni.arvanitis@me.com>
2018-02-02 12:31:03 -0500

    added recs for /

---
+++ client/recs.js
@@ -0,0 +1,24 @@
+const recs = [
+  [
+    `Eleni and I worked together making our first e-commerce website and she has been fantastic to work with. She was integral to our deployment and our oAuth integration. She brought great enthusiasm and a positive attitude to our group every day while still being an earnestly hard worker. Eleni Arvanitis is an excellent programmer with a great mind for engineering.`,
+    '- Keri'
+  ],
+  [
+    `Eleni is very passionate about computer programming. [S]he is a super quick learner ... As a teammate, she cares if our team members are all on the same page when our app is moving through serial code challenges. She takes responsibility for the features that we were building together and openly communicates her thoughts. It was great working with Eleni, and I highly recommend building apps with her!`,
+    '- Guang'
+  ],
+  [
+    `Eleni Arvanitis was one of the most driven and enthusiastic people I have had the pleasure to work with. As a teammate she set a high standard and motivated all of us to create a product [of which] we are so very proud.`,
+    '- Christina'
+  ],
+  [
+    `Very passionate and driven to succeed! I loved working with Eleni on our capstone project and hearing her ideas for how things should be implemented. She is not afraid to learn new technologies and can often do so quickly and efficiently. Great experience working with her!`,
+    '- Samantha'
+  ],
+  [
+    `I had the pleasure of working with Eleni on a recent project. She is a bright, charismatic team player who kept us on track... She is very passionate about everything she does and it truly shows when pairing with her. I know she will excel [at] anything she sets her mind.`,
+    '- Cecilia'
+  ],
+]
+
+export default recs
