54c871d85db2d1eea7c7aa6c80be30a8e290206f
Eleni A <eleni.arvanitis@me.com>
2018-02-04 20:55:17 -0500

    cleanup

--- client/components/Articles.js
+++ client/components/Articles.js
@@ -1,23 +1,18 @@
 import React from 'react';
+
 import '~/public/assets/styles/articles.css'
 import articles from '~/content/articles'
 
-export default () => {
-  return <div className='all-articles'>
-    {
-      articles.map(article => {
-        return <div key={article[0]} className='article'>
-          <h3 className='article-title'>{article[0]}</h3>
-          <div>
-            <p className='date-posted'>{article[1]}</p>
-            <p className='description'>{article[2]}</p><br/>
-            <a href={article[3]} target='_blank'>
-              <button>Read More ↗</button>
-            </a>
-            <hr/>
-          </div>
-        </div>
-      })
-    }
-  </div>
-}
+export default () => <div className='all-articles'>
+  {
+    articles.map(article => <div key={article[0]} className='article'>
+        <h3 className='article-title'>{article[0]}</h3>
+        <p className='date-posted'>{article[1]}</p>
+        <p className='description'>{article[2]}</p><br/>
+        <a href={article[3]} target='_blank'>
+          <button>Read More ↗</button>
+        </a>
+        <hr/>
+      </div>)
+  }
+</div>
