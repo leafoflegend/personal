7c0e6e72119e3a558c83effb7a07f36217eff3b2
Eleni A <eleni.arvanitis@me.com>
2018-02-04 21:09:34 -0500

    smarty pants component setup

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReCAPTCHA from 'react-google-recaptcha'
 
 import { siteKey, secretKey } from '~/content/secrets'
 import '~/public/assets/styles/contact.css'
 
-export default () => <div className='contact'>
-  <h2>Let's Chat!</h2>
-  <div className='contact-form'>
-    <label className='form-label'>
-      <input className='form-input' type='text' name='name'
-             placeholder='* Name' required/><br/>
-      <input className='form-input' type='email' name='email'
-             placeholder='* Email' required/><br/>
-      <textarea className='form-input' name='message'
-                placeholder='* Message'/><br/>
-      <ReCAPTCHA ref='recaptcha' sitekey={siteKey} theme='dark'/>
-      <button>Submit</button>
-    </label>
-  </div>
-</div>
+export default class Contact extends Component {
+  render() {
+    return <div className='contact'>
+      <h2>Let's Chat!</h2>
+      <div className='contact-form'>
+        <label className='form-label'>
+          <input className='form-input' type='text' name='name'
+                 placeholder='* Name' required/><br/>
+          <input className='form-input' type='email' name='email'
+                 placeholder='* Email' required/><br/>
+          <textarea className='form-input' name='message'
+                    placeholder='* Message'/><br/>
+          <ReCAPTCHA ref='recaptcha' sitekey={siteKey} theme='dark'/>
+          <button>Submit</button>
+        </label>
+      </div>
+    </div>
+  }
+}
