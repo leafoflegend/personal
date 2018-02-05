257b7f3b70a5339aee4f2d12ae72272bb62a2e66
Eleni A <eleni.arvanitis@me.com>
2018-02-04 20:01:58 -0500

    adj && cleanup && better keys

--- client/components/Home.js
+++ client/components/Home.js
@@ -12,31 +12,29 @@ const me = [
   'a gamer',
   'innovative',
   'a Ravenclaw',
-  'intuitive',
+  'inventive',
 ]
 
-export default () => {
-  return <div className='me'>
-    <div className='text-carousel'>
-      I am
-        {
-          me.map((descriptor, i) => {
-            return <span className='carousel-items' key={i}>
-                {descriptor}
-            </span>
-          })
-        }
-      <NavLink to='/contact'>
-        <button>Say hi</button>
-      </NavLink>
-    </div>
-    <div className='rec'>
+export default () => <div className='me'>
+  <div className='text-carousel'>
+    I am
       {
-        recs.map(rec => <div key={rec[1]}>
-          <p className='rec-content'>"{rec[0]}"</p>
-          <p className='rec-name'>{rec[1]}</p>
-        </div>)
+        me.map(descriptor => {
+          return <span className='carousel-items' key={descriptor}>
+              {descriptor}
+          </span>
+        })
       }
-    </div>
+    <NavLink to='/contact'>
+      <button>Say hi</button>
+    </NavLink>
   </div>
-}
+  <div className='rec'>
+    {
+      recs.map(rec => <div key={rec[1]}>
+        <p className='rec-content'>"{rec[0]}"</p>
+        <p className='rec-name'>{rec[1]}</p>
+      </div>)
+    }
+  </div>
+</div>
