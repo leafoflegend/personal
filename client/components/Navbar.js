830e8c74607b4185587054d10a6f34ef42bbe9bc
Eleni A <eleni.arvanitis@me.com>
2018-02-04 21:09:23 -0500

    less lines, same fun

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
 import '~/public/assets/styles/navbar.css'
 
-const logo = '/assets/img/ea-logo-cream.png';
+const logo = '/assets/img/ea-logo-cream.png'
+    , nav = ['about', 'work', 'articles', 'contact']
 
-// TODO: Remove home constant color selection
-
-export default () => {
-  return <nav className="nav-bar">
-    <NavLink to='/'>
-      <img className="logo" src={logo} alt="ea-logo-cream"/>
-    </NavLink>
-    {/*<NavLink to='/' className='underline'>Home</NavLink>*/}
-    <NavLink to='/about' className='underline'>About</NavLink>
-    <NavLink to='/work' className='underline'>Work</NavLink>
-    <NavLink to='/articles' className='underline'>Articles</NavLink>
-    <NavLink to='/contact' className='underline'>Contact</NavLink>
-  </nav>
-}
+export default () => <nav className="nav-bar">
+  <NavLink to='/'>
+    <img className="logo" src={logo} alt="ea-logo-cream"/>
+  </NavLink>
+  {
+    nav.map(link => {
+      return <NavLink key={link} to={`/${link}`} className='underline'>
+        {`${link[0].toUpperCase()}${link.slice(1)}`}
+      </NavLink>
+    })
+  }
+</nav>
