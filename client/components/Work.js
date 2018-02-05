b4f1dd519a9138a96011916e2a25a02b27968ea8
Eleni A <eleni.arvanitis@me.com>
2018-02-04 19:51:27 -0500

    added recs and projects info

--- client/components/Work.js
+++ client/components/Work.js
@@ -1,31 +1,19 @@
 import React from 'react'
 import '~/public/assets/styles/work.css'
-
-const projects = [
-  'coquill',
-  'by land or sea',
-  "'what is my purpose?'",
-  'witcher school of code',
-  'martinsville deli',
-  'npm library',
-  'game'
-]
+import projects from '~/content/projects'
 
 export default () => {
   window.scrollTo(0, 0)
 
   return <div className='main-project-div'>
-    <a className='button-link'
-       href='/assets/Eleni-Arvanitis-Resume.pdf'
+    <a href='/assets/Eleni-Arvanitis-Resume.pdf'
        target='_blank'>
       <button className='resume-button'>View Resume</button>
     </a>
     {
       projects.map(project => {
-        return <div key={project} className='projects'>
-          <span className='line'>
-            {project.toUpperCase()}
-          </span>
+        return <div key={project[0]} className='projects'>
+          <span className='line'>{project[0].toUpperCase()}</span>
         </div>
       })
     }
---
+++ content/projects.js
@@ -0,0 +1,109 @@
+const projects = [
+  [
+    'coquill',
+    {
+      description: `An in-browser real-time collaborative text editor for screenplays.`,
+      role: `Engineer:
+      Firebase database setup with immutable.js, frontend design, and sentiment analysis`,
+      technologies: [
+        'firebase',
+        'immutable.js',
+        'react', 'redux', 'react-redux',
+        'sentiment analysis',
+      ],
+      links: [
+        'https://coquill-e559a.firebaseapp.com/',
+        'https://github.com/queenplay-associates/coquill',
+      ],
+    },
+  ],
+  [
+    'by land or sea',
+    {
+      description: `So you want to go on vacation but don't know where you want to go? Ask "By Land or Sea," a smart voice-enabled app for all voice assistants made with the Google Assistant's SDK. Have a conversation with the assistant, and when you're done, she'll give you three options from which to choose. Book your vacation with the sound of your voice or a quick response to the chat-bot. Happy travels!`,
+      role: 'Solo Engineer',
+      technologies: [
+        'dialogflow',
+        'actions on google',
+        'google assistant sdk',
+        'stripe',
+      ],
+      links: ['https://github.com/datgreekchick/travel'],
+    },
+  ],
+  [
+    "'what is my purpose?'",
+    {
+      description: `In one day, I created a robot inspired by the butter bot in "Rick and Morty." I used a kit to build the robot with parts and hooked the bot up to an Arduino. For speak purposes, I utilized Alexa Skills Kit to create a lambda function that would model the robot's responses after those in the show's episode (S1 E09). In the future, I will implement the ClarifAI API so that the robot will be able to use its camera to learn whether or not there is a human in front of it. If so, the butter bot will automatically engage in conversation, namely wondering what its purpose is in the world.`,
+      role: 'Solo Engineer',
+      technologies: [
+        'arduino',
+        'alexa sdk',
+        'lambda functions',
+        'alexa skills kit',
+        'clarifAI API',
+      ],
+      links: [
+        'https://github.com/DatGreekChick/purpose',
+        'https://www.youtube.com/watch?v=0mvH3GBVqtw'
+      ]
+    },
+  ],
+  [
+    'witcher school of code',
+    {
+      description: `A place where witchers no longer need to make coin by killing monsters.`,
+      role: 'Solo Engineer',
+      technologies: [
+        'react', 'redux', 'react-redux',
+        'sequelize',
+        'axios',
+      ],
+      links: ['https://github.com/datgreekchick/witcher'],
+    },
+  ],
+  [
+    'martinsville deli',
+    {
+      description: `I took a pre-existing one-page website and turned it into an e-commerce platform worthy of the company's customers. Previously, the code was only placed in the one html file, but this, like all other projects of mine, focuses on best practices and clean, modular code. Works are in place for an app to make online ordering even more seamless and accessible.`,
+      role: 'Solo Engineer',
+      technologies: [
+        'stripe',
+        'firebase',
+        '',
+      ],
+      links: [
+        'https://martinsvilledeli.com',
+        'https://github.com/datgreekchick/MD-Website',
+      ],
+    },
+  ],
+  [
+    'chrome extension',
+    {
+      description: ``,
+      role: 'Engineer',
+      technologies: [
+        '',
+      ],
+      links: [
+        ''
+      ],
+    },
+  ],
+  [
+    'game',
+    {
+      description: ``,
+      role: 'Engineer',
+      technologies: [
+        '',
+      ],
+      links: [
+        ''
+      ],
+    },
+  ],
+]
+
+export default projects
