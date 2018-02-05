c3aa9a7dfbd2bec13d78bd5c9aa04a1468ade64c
Eleni A <eleni.arvanitis@me.com>
2018-02-04 20:48:29 -0500

    moved articles out to its own file

--- client/components/Articles.js
+++ client/components/Articles.js
@@ -1,22 +1,6 @@
 import React from 'react';
 import '~/public/assets/styles/articles.css'
-
-// TODO: Store articles in firebase db to access links (button), titles (top, right from photo), date created, description
-
-const links = [
-  [
-    '"Alexa, Can You Hear Me?"',
-    '19 November 2017',
-    'In this article, I touch upon the Machine Learning model for personal assistants like Alexa, their pre-programmed abilities, and the impact of how we increasingly rely on our machines as friends.',
-    'https://medium.com/@datgreekchick/alexa-can-you-hear-me-cf43e820534c',
-  ],
-  [
-    'Stop Killing Animals for the Gram',
-    '18 January 2018',
-    'A quick plea: If you\'re visiting Africa, avoid companies that offer the experience to pet or walk exotic animals. Those companies breed or trap them for profit - including that of selling them to be easily killed upon "release."',
-    'https://medium.com/@datgreekchick/stop-killing-animals-for-the-gram-257014f3de83',
-  ],
-];
+import articles from '~/content/articles'
 
 export default () => {
   return <div className='all-articles'>
---
+++ content/articles.js
@@ -0,0 +1,16 @@
+const articles = [
+  [
+    'Alexa, Can You Hear Me?',
+    '19 November 2017',
+    'In this article, I touch upon the Machine Learning model for personal assistants like Alexa, their pre-programmed abilities, and the impact of how we increasingly rely on our machines as friends.',
+    'https://medium.com/@datgreekchick/alexa-can-you-hear-me-cf43e820534c',
+  ],
+  [
+    'Stop Killing Animals for the Gram',
+    '18 January 2018',
+    'A quick plea: If you\'re visiting Africa, avoid companies that offer the experience to pet or walk exotic animals. Those companies breed or trap them for profit - including that of selling them to be easily killed upon release.',
+    'https://medium.com/@datgreekchick/stop-killing-animals-for-the-gram-257014f3de83',
+  ],
+]
+
+export default articles
