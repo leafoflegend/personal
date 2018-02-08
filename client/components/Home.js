8a7b8604829993334e74eaaaf3897c9635cf8aa5
Eleni A <eleni.arvanitis@me.com>
2018-02-08 12:33:39 -0500

    text carousel ftw

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,40 +1,26 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import TextLoop from 'react-text-loop'
 
 import '~/public/assets/styles/home.css'
-import recs from '~/content/recs'
-
-const me = [
-  'a software engineer',
-  'imaginative',
-  'a teaching fellow',
-  'inquisitive',
-  'a gamer',
-  'innovative',
-  'a Ravenclaw',
-  'inventive',
-]
+import { me, description} from '~/content/about'
 
 export default () => <div className='me'>
   <div className='text-carousel'>
     I am
+    <TextLoop>
       {
         me.map(descriptor => {
-          return <span className='carousel-items' key={descriptor}>
+          return <span className='carousel-items'
+                       key={descriptor}>
               {descriptor}
           </span>
         })
       }
+    </TextLoop>
+    <p className='home-description'>{description}</p>
     <NavLink to='/contact'>
       <button>Say hi</button>
     </NavLink>
-  </div>
-  <div className='rec'>
-    {
-      recs.map(rec => <div key={rec[1]}>
-        <p className='rec-content'>"{rec[0]}"</p>
-        <p className='rec-name'>- {rec[1]}</p>
-      </div>)
-    }
   </div>
 </div>
