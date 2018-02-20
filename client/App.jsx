35c88cec99888988046be09da5541d806339000c
Eleni A <eleni.arvanitis@me.com>
2018-02-19 21:24:29 -0500

    back to dumb and exact paths are gone from most routes

--- client/App.jsx
+++ client/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 
 import '~/public/assets/styles/index.css'
@@ -12,38 +12,16 @@ import Articles from '~/client/components/Articles'
 import Contact from '~/client/components/Contact'
 import Footer from '~/client/components/Footer'
 
-// import firebase from 'firebase'
-
-export default class App extends Component {
-  state = { contacts: [] }
-
-  // addContact = ({ name, email, msg }) => {
-    // axios.post('/api/contacts', { name, email, msg })
-    //   .then(res => res.data)
-    //   .then(contactInfo => {
-    //     this.setState({ contacts: [...this.state.contacts, contactInfo] })
-    //   });
-  // }
-
-  // componentDidMount() {
-  //   axios.get('/api/contacts')
-  //     .then(res => res.data)
-  //     .then(contacts => this.setState({ contacts }));
-  // }
-
-  render() {
-    return <Router>
-      <div>
-        <Navbar/>
-        <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route exact path="/about" component={About}/>
-          <Route exact path='/work' component={Work}/>
-          <Route exact path='/articles' component={Articles}/>
-          <Route exact path="/contact" component={Contact}/>
-        </Switch>
-        <Footer/>
-      </div>
-    </Router>
-  }
-}
+export default () => <Router>
+  <div>
+    <Navbar/>
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path='/work' component={Work}/>
+      <Route path='/articles' component={Articles}/>
+      <Route path="/contact" component={Contact}/>
+    </Switch>
+    <Footer/>
+  </div>
+</Router>
